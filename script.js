3067d540554659111e55c6e600c984d07c7c89c8
wojtich <wojtich@gmail.com>
2019-04-02 16:23:09 +0000

    Fetch

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
-var header = document.querySelector("header");
-
 const url = "https://api.openweathermap.org/data/2.5/weather?";
 const apiKey = "6b6e4de508acbad1c47bc291a27a6d99";
-let unit = "metric";
-let language = "pl";
+const unit = "metric";
+const language = "pl";
+
+let weather;
 
 function getWeather() {
-  let city = document.getElementById("city").value;
+  const city = document.getElementById("city").value;
   const API = url + "q=" + city + "&appid=" + apiKey + "&units=" + unit + "&lang=" + language;
 
-  var request = new XMLHttpRequest();
-  request.open("GET", API);
-  request.responseType = "json";
-  request.send();
-  request.onload = function() {
-    var weather = request.response;
-    showWeather(weather);
-  };
+  fetch(API)
+    .then(
+      function (response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+        response.json().then(function (data) {
+          weather = data;
+          console.log(weather);
+          showWeather(weather);
+        });
+      }
+    )
+
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    })
 }
 
 function showWeather(jsonObj) {
-  var myH1 = document.querySelector("h1");
-  myH1.textContent = jsonObj.name;
+  document.querySelector("h1").textContent = jsonObj.name;
 
-  var myPara = document.querySelector("p");
-  myPara.textContent = "temperatura: " + jsonObj.main.temp;
+  document.querySelector("p").textContent = "temperatura: " + jsonObj.main.temp;
 }
 
 getWeather();
 
-let button = document.getElementById("submit");
+const button = document.getElementById("submit");
 button.addEventListener("click", getWeather);
