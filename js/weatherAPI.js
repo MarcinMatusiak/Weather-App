78560ce4d1d7b646bd969366be983af60e6fdd65
wojtich <wojtich@gmail.com>
2019-04-06 17:37:30 +0000

    Update weatherAPI.js

--- js/weatherAPI.js
+++ js/weatherAPI.js
@@ -4,9 +4,8 @@ const unit = "metric";
 const language = "pl";
 
 
-function getWeather() {
-    const city = document.getElementById("city").value;
-    const API = url + "q=" + city + "&appid=" + apiKey + "&units=" + unit + "&lang=" + language;
+function getWeather(city) {
+    const API = `${url}q=${city}&appid=${apiKey}&units=${unit}&lang=${language}`;
 
     return fetch(API)
         .then(response => response.json())
