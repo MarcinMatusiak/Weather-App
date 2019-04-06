f0d5ec20e24b0c7a9f7c9c47acfdddb457208688
wojtich <wojtich@gmail.com>
2019-04-06 13:18:43 +0000

    Upload weatherAPI.js file

---
+++ js/weatherAPI.js
@@ -0,0 +1,18 @@
+const url = "https://api.openweathermap.org/data/2.5/weather?";
+const apiKey = "6b6e4de508acbad1c47bc291a27a6d99";
+const unit = "metric";
+const language = "pl";
+
+
+function getWeather() {
+    const city = document.getElementById("city").value;
+    const API = url + "q=" + city + "&appid=" + apiKey + "&units=" + unit + "&lang=" + language;
+
+    return fetch(API)
+        .then(response => response.json())
+        .catch(function (err) {
+            console.log('Fetch Error :-S', err);
+        })
+}
+
+export default getWeather;
