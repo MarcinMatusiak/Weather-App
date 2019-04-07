c02bb783d5e35aef702a45a548021cc0a10bd934
wojtich <wojtich@gmail.com>
2019-04-07 18:16:43 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 import getWeather from './weatherAPI.js';
 
 function showWeather(jsonObj) {
-  document.querySelector("h2").textContent = jsonObj.name;
-  document.querySelector("p").textContent = "temperatura: " + jsonObj.main.temp;
+  document.querySelector('h2').textContent = jsonObj.name;
+  document.querySelector('p').textContent = 'temperatura: ' + jsonObj.main.temp;
 }
 
-const button = document.getElementById("submit");
-button.addEventListener("click", loadWeather);
+const button = document.getElementById('submit');
+button.addEventListener('click', loadWeather);
 
 function loadWeather() {
-  getWeather(document.getElementById("city").value)
+  getWeather(document.getElementById('city').value)
     .then(result => {
       showWeather(result);
     })
